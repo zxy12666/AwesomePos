d0c5b5f6551bea3504728634da61fa218944e73b
zhangxiangyu <zhangxiangyu@jianhuabm.com>
2018-02-06 17:00:12 +0800

    Initial commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="left-nav">
+    <ul>
+      <li>
+        <div >
+          <svg  class="icon" aria-hidden="true"  >
+            <use xlink:href="#icon-wodezichan"></use>
+          </svg>
+        </div>
+
+        <div>收银</div>
+      </li>
+      <li>
+        <svg class="icon " aria-hidden="true"  >
+          <use xlink:href="#icon-31dianpu"></use>
+        </svg>
+        <div>店铺</div>
+      </li>
+      <li>
+        <svg class="icon " aria-hidden="true"  >
+          <use xlink:href="#icon-hanbao"></use>
+        </svg>
+        <div>商品</div>
+      </li>
+      <li>
+        <svg class="icon " aria-hidden="true"  >
+          <use xlink:href="#icon-31huiyuanqia"></use>
+        </svg>
+        <div>会员</div>
+      </li>
+      <li>
+        <svg class="icon " aria-hidden="true"  >
+          <use xlink:href="#icon-jifen"></use>
+        </svg>
+        <div>统计</div>
+      </li>
+      <li>
+        <svg class="icon " aria-hidden="true"  >
+          <use xlink:href="#icon-31shezhi"></use>
+        </svg>
+        <div>设置</div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style>
+
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pos">
+    <el-row>
+      <el-col :span="7" class="pos-order">
+          <div class="grid-content bg-purple" id="order-list">
+            我是订单栏
+          </div>
+      </el-col>
+      <el-col >
+          <div class="grid-content bg-purple-light">
+              我是产品栏
+          </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pos',
+    mounted:function(){
+      var orderHeight=document.body.clientHeight;
+      document.getElementById("order-list").style.height=orderHeight+'px';
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
